45918eb8a11b150e8702435da40c39a47fefe438
dankelleher <dankelleher@yahoo.com>
2018-03-03 13:15:52 +0100

    Bugfixes and rudimentary error handling

--- proxy.js
+++ proxy.js
@@ -1,16 +1,26 @@
+const serializeError = require('serialize-error');
+
 function handler(event, context, callback) {
   // extract the path to the handler (relative to the project root)
   // and the function to call on the handler
   const [targetHandlerFile, targetHandlerFunction] = event.targetHandler.split(".");
   const target = require('../../' + targetHandlerFile);
 
-  // remove all fields from the incoming event leaving just the body
-  const newEvent = {
-    body: event.body
-  };
-
   // call the target function
-  target[targetHandlerFunction](newEvent, context, callback);
+  target[targetHandlerFunction](event.body, context, (error, response) => {
+    if (error) {
+      callback(null, {
+        StatusCode: 500,
+        FunctionError: 'Handled',
+        Payload: serializeError(error)
+      })
+    } else {
+      callback(null, {
+        StatusCode: 200,
+        Payload: JSON.stringify(response)
+      })
+    }
+  });
 }
 
 module.exports.handler = handler;
