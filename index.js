ac83a0f3242489762f3df0ef02001fed64110b69
Martin Jesper Low Madsen <mj@blackbird.online>
2018-07-05 16:10:43 +0200

    Fixes usage with serverless-webpack and serverless-offline hooks

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const packagePath = 'node_modules/serverless-offline-direct-lambda';
-const handlerPath = `proxy.js`;
+const handlerPath = 'proxy.js';
 
 class ServerlessPlugin {
   constructor(serverless, options) {
@@ -9,31 +9,37 @@ class ServerlessPlugin {
     this.options = options;
 
     this.hooks = {
-      "before:offline:start:init": this.startHandler.bind(this),
+      'before:offline:start': this.startHandler.bind(this),
     };
   }
 
   startHandler() {
+    let location = '';
+    try {
+      location = this.serverless.service.custom['serverless-offline'].location;
+      this.serverless.service.custom['serverless-offline'].location = '';
+    } catch (_) { }
+
     this.serverless.cli.log('Running Serverless Offline with direct lambda support');
 
-    addProxies(this.serverless.service.functions);
+    addProxies(this.serverless.service.functions, location);
   }
 }
 
-const addProxies = functionsObject => {
+const addProxies = (functionsObject, location) => {
   Object.keys(functionsObject).forEach(fn => {
 
     // filter out functions with event config,
     // leaving just those intended for direct lambda-to-lambda invocation
     const functionObject = functionsObject[fn];
-    if (!functionObject.events || functionObject.events.length == 0) {
-      const pf = functionProxy(functionObject);
+    if (!functionObject.events || !functionObject.events.some((event) => event === 'http')) {
+      const pf = functionProxy(functionObject, location);
       functionsObject[pf.name] = pf;
     }
   });
 };
 
-const functionProxy = functionBeingProxied => ({
+const functionProxy = (functionBeingProxied, location) => ({
   name: `${functionBeingProxied.name}_proxy`,
   handler: `${packagePath}/proxy.handler`,
   events: [
@@ -46,8 +52,9 @@ const functionProxy = functionBeingProxied => ({
           template: {
             'application/json': JSON.stringify(
               {
+                location,
+                body: "$input.json('$')",
                 targetHandler :  functionBeingProxied.handler,
-                body: "$input.json('$')"
               }
             )
           }
--- proxy.js
+++ proxy.js
@@ -1,24 +1,25 @@
 const serializeError = require('serialize-error');
+const path = require('path');
 
-function handler(event, context, callback) {
+async function handler(event, context, callback) {
   // extract the path to the handler (relative to the project root)
   // and the function to call on the handler
-  const [targetHandlerFile, targetHandlerFunction] = event.targetHandler.split(".");
-  const target = require('../../' + targetHandlerFile);
+  const [targetHandlerFile, targetHandlerFunction] = event.targetHandler.split('.');
+  const target = require(path.resolve(__dirname, '../..', event.location, targetHandlerFile));
 
   // call the target function
-  target[targetHandlerFunction](event.body, context, (error, response) => {
+  return target[targetHandlerFunction](event.body, context, (error, response) => {
     if (error) {
       callback(null, {
         StatusCode: 500,
         FunctionError: 'Handled',
-        Payload: serializeError(error)
-      })
+        Payload: serializeError(error),
+      });
     } else {
       callback(null, {
         StatusCode: 200,
-        Payload: JSON.stringify(response)
-      })
+        Payload: JSON.stringify(response),
+      });
     }
   });
 }
