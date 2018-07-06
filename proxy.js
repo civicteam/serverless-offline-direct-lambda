38f5c952e1133a54dbbdd1277d6ca2b6c55d84c2
Martin Jesper Low Madsen <mj@blackbird.online>
2018-07-06 14:12:26 +0200

    Further mimics a real Lambda invocation where body contains the invocation parameters

--- proxy.js
+++ proxy.js
@@ -2,13 +2,21 @@ const serializeError = require('serialize-error');
 const path = require('path');
 
 async function handler(event, context, callback) {
+  const { ClientContext, FunctionName, InvocationType, LogType, Payload } = event.body;
+
   // extract the path to the handler (relative to the project root)
   // and the function to call on the handler
   const [targetHandlerFile, targetHandlerFunction] = event.targetHandler.split('.');
   const target = require(path.resolve(__dirname, '../..', event.location, targetHandlerFile));
 
+  const targetEvent = JSON.parse(Payload);
+  const targetContext = {
+    ...context,
+    clientContext: JSON.parse(Buffer.from(ClientContext, 'base64')),
+  };
+
   // call the target function
-  return target[targetHandlerFunction](event.body, context, (error, response) => {
+  return target[targetHandlerFunction](targetEvent, targetContext, (error, response) => {
     if (error) {
       callback(null, {
         StatusCode: 500,
